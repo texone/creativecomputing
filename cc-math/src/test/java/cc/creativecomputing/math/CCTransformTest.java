ecd04a2d5a8cc740a99c1d50a2e32b7863b16f58
christian riekoff <info@texone.org>
2015-12-14 09:00:51 +0100

    fixed transform test

--- cc-math/src/test/java/cc/creativecomputing/math/CCTransformTest.java
+++ cc-math/src/test/java/cc/creativecomputing/math/CCTransformTest.java
@@ -1,22 +1,22 @@
-///**
-// * Copyright (c) 2008-2012 Ardor Labs, Inc.
-// *
-// * This file is part of Ardor3D.
-// *
-// * Ardor3D is free software: you can redistribute it and/or modify it 
-// * under the terms of its license which may be found in the accompanying
-// * LICENSE file or at <http://www.ardor3d.com/LICENSE>.
-// */
-//
-//package cc.creativecomputing.math;
-//
-//import static org.junit.Assert.*;
-//
-//import java.nio.FloatBuffer;
-//
-//import org.junit.Test;
-//
-//public class CCTransformTest {
+/**
+ * Copyright (c) 2008-2012 Ardor Labs, Inc.
+ *
+ * This file is part of Ardor3D.
+ *
+ * Ardor3D is free software: you can redistribute it and/or modify it 
+ * under the terms of its license which may be found in the accompanying
+ * LICENSE file or at <http://www.ardor3d.com/LICENSE>.
+ */
+
+package cc.creativecomputing.math;
+
+import static org.junit.Assert.*;
+
+import java.nio.FloatBuffer;
+
+import org.junit.Test;
+
+public class CCTransformTest {
 //
 //	@Test
 //	public void testGetSet() {
@@ -429,4 +429,4 @@
 //			assertTrue(val == fb.get());
 //		}
 //	}
-//}
+}
