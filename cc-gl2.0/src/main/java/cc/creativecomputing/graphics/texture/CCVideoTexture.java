66bce44ebb8b850f9dd761568582240778039a87
christian riekoff <info@texone.org>
2016-05-24 09:54:30 +0200

    fixed video texture

--- cc-gl2.0/src/main/java/cc/creativecomputing/graphics/texture/CCVideoTexture.java
+++ cc-gl2.0/src/main/java/cc/creativecomputing/graphics/texture/CCVideoTexture.java
@@ -10,6 +10,10 @@
  */
 package cc.creativecomputing.graphics.texture;
 
+import cc.creativecomputing.gl.app.CCGLListener;
+import cc.creativecomputing.graphics.CCGraphics;
+import cc.creativecomputing.graphics.app.CCGL2Adapter;
+import cc.creativecomputing.image.CCImage;
 import cc.creativecomputing.video.CCVideo;
 import cc.creativecomputing.video.CCVideoTextureDataListener;
 
@@ -17,42 +21,74 @@ import cc.creativecomputing.video.CCVideoTextureDataListener;
  * @author christianriekoff
  *
  */
-public class CCVideoTexture<VideoType extends CCVideo> extends CCTexture2D implements CCVideoTextureDataListener{
+public class CCVideoTexture extends CCTexture2D implements CCVideoTextureDataListener, CCGLListener<CCGraphics>{
 	
-	private VideoType _myMovie;
+	private CCVideo _myMovie;
 	
-	public CCVideoTexture(VideoType theData, CCTextureTarget theTarget, final CCTextureAttributes theAttributes) {
+	public CCVideoTexture(CCGL2Adapter theAdapter, CCVideo theData, CCTextureTarget theTarget, final CCTextureAttributes theAttributes) {
 		super(theTarget, theAttributes, 1,800,200);
 		video(theData);
+		theAdapter.glListener().add(this);
 	}
 	
-	public CCVideoTexture(VideoType theData){
+	public CCVideoTexture(CCGL2Adapter theAdapter,final CCTextureTarget theTarget, CCTextureAttributes theAttributes) {
+		super(theTarget, theAttributes, 1,800,200);
+		theAdapter.glListener().add(this);
+	}
+	
+	public CCVideoTexture(CCVideo theData){
 		super(theData);
 		video(theData);
 	}
 	
-	public void video(VideoType theData){
+	public void video(CCVideo theData){
 		if(_myMovie != null)_myMovie.removeListener(this);
 		_myMovie = theData;
 		_myMovie.addListener(this);
 	}
 	
-	public VideoType video(){
+	public CCVideo video(){
 		return _myMovie;
 	}
+	
+	private CCImage _myInitVideo = null;
 
 	/* (non-Javadoc)
 	 * @see cc.creativecomputing.texture_new.video.CCVideoTextureData.CCVideoTextureDataListener#onInit(cc.creativecomputing.texture_new.video.CCVideoTextureData)
 	 */
-	public void onInit(CCVideo theData) {
-		data(theData);
+	public void onInit(CCImage theData) {
+		_myInitVideo = theData;
 	}
+	
+	private CCImage _myUpdateVideo = null;
 
 	/* (non-Javadoc)
 	 * @see cc.creativecomputing.texture_new.video.CCVideoTextureData.CCVideoTextureDataListener#onUpdate(cc.creativecomputing.texture_new.video.CCVideoTextureData)
 	 */
-	public void onUpdate(CCVideo theData) {
-		updateData(theData);
+	public void onUpdate(CCImage theData) {
+		_myUpdateVideo = theData;
+	}
+
+	@Override
+	public void reshape(CCGraphics theContext) {}
+
+	@Override
+	public void init(CCGraphics theContext) {}
+
+	@Override
+	public void dispose(CCGraphics theContext) {}
+
+	@Override
+	public void display(CCGraphics theContext) {
+		if(_myInitVideo != null){
+			data(_myInitVideo);
+			_myInitVideo = null;
+		}
+		if(_myUpdateVideo != null){
+			System.out.println(_myUpdateVideo.internalFormat());
+			data(_myUpdateVideo);
+			_myUpdateVideo = null;
+		}
 	}
 
 }
