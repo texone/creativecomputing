87f0a46256de89b658946a04665e5bb4874e6dd2
christian riekoff <info@texone.org>
2017-01-21 13:35:11 +0100

    fixed tcp client and server class

--- cc-net/src/main/java/cc/creativecomputing/io/netty/CCTCPClient.java
+++ cc-net/src/main/java/cc/creativecomputing/io/netty/CCTCPClient.java
@@ -1,13 +1,56 @@
 package cc.creativecomputing.io.netty;
 
-import io.netty.channel.socket.nio.NioSocketChannel;
+import java.net.InetSocketAddress;
+
+import cc.creativecomputing.core.logging.CCLog;
 import cc.creativecomputing.io.netty.codec.CCNetCodec;
 import cc.creativecomputing.io.netty.codec.CCNetStringCodec;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
 
 public class CCTCPClient<MessageType> extends CCClient<MessageType>{
+	protected final String _myHost;
 
 	public CCTCPClient(CCNetCodec<MessageType> theCodec, String theHost, int thePort) {
-		super(NioSocketChannel.class, theCodec, theHost, thePort);
+		super(theCodec, thePort);
+		_myHost = theHost;
+	}
+	
+	
+	@Override
+	public void createBootstrap(){
+		Bootstrap myBootstrap = new Bootstrap();
+		myBootstrap.group(_myGroup);
+		myBootstrap.channel(NioSocketChannel.class);
+		myBootstrap.remoteAddress(new InetSocketAddress(_myHost, _myPort));
+		myBootstrap.handler(new ChannelInitializer<SocketChannel>() {
+
+			@Override
+			public void initChannel(SocketChannel ch) throws Exception {
+				ch.pipeline().addLast(_myCodec.decoder());
+				ch.pipeline().addLast(_myCodec.encoder(), new CCClientHandler());
+			}
+		});
+		try {
+			_myFuture = myBootstrap.connect();
+			
+			if(_myReconnectTime > 0){
+				_myFuture.addListener((channelFuture) -> {
+					if (_myFuture.isSuccess())return;
+
+					CCLog.info("SCHEDULE RECONNECT");
+					scheduleReconnect(_myFuture.channel().eventLoop());
+				});
+			}
+			
+			_myFuture.sync();
+			_myIsConnected = true;
+		} catch (Exception e) {
+			e.printStackTrace();
+//			throw new CCNetException(e);
+		}
 	}
 
 	public static void main(String[] args) throws Exception {
