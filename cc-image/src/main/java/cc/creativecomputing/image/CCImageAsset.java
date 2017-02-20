7172b82f00a206387627968f78dc0af13d608a98
christian riekoff <info@texone.org>
2017-02-20 20:55:10 +0100

    cleaned image asset

--- cc-image/src/main/java/cc/creativecomputing/image/CCImageAsset.java
+++ cc-image/src/main/java/cc/creativecomputing/image/CCImageAsset.java
@@ -1,8 +1,6 @@
 package cc.creativecomputing.image;
 
 import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.Map;
 
 import cc.creativecomputing.control.CCAsset;
 import cc.creativecomputing.image.CCImage;
@@ -10,32 +8,13 @@ import cc.creativecomputing.image.CCImageIO;
 
 public class CCImageAsset extends CCAsset<CCImage>{
 	
-	
-	private Map<Path, CCImage> _myImageMap = new HashMap<>();
-	
-	private Path _myPath;
-	
 	public CCImageAsset(){
 		_myAsset = new CCImage();
 	}
-
+	
 	@Override
-	public void onChangePath(Path thePath) {
-		if(thePath == null){
-			_myPath = thePath;
-			_myAsset = null;
-			return;
-		}
-		if(_myImageMap.containsKey(thePath)){
-			_myAsset = _myImageMap.get(thePath);
-			return;
-		}
-		try{
-			_myAsset = CCImageIO.newImage(thePath);
-			_myImageMap.put(_myPath, _myAsset);
-		}catch(Exception e){
-			
-		}
+	public CCImage loadAsset(Path thePath) {
+		return CCImageIO.newImage(thePath);
 	}
 	
 	@Override
