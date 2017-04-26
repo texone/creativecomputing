a037b5368246a2dba521c49f3f75286726568da2
christian riekoff <info@texone.org>
2017-04-26 10:29:33 +0200

    added displayble interface and graphics object

---
+++ cc-gl2.0/src/main/java/cc/creativecomputing/graphics/CCDisplayable.java
@@ -0,0 +1,6 @@
+package cc.creativecomputing.graphics;
+
+public interface CCDisplayable {
+
+	public void display();
+}
---
+++ cc-gl2.0/src/main/java/cc/creativecomputing/graphics/CCGraphicObject.java
@@ -0,0 +1,17 @@
+package cc.creativecomputing.graphics;
+
+import cc.creativecomputing.app.modules.CCAnimator;
+import cc.creativecomputing.control.code.CCRuntimeCompilable;
+import cc.creativecomputing.graphics.app.CCGL2Adapter;
+
+public class CCGraphicObject implements CCRuntimeCompilable{
+
+	public void init(CCGL2Adapter theApp, CCGraphics g, CCAnimator theAnimator) {
+	}
+
+	public void update(CCAnimator theAnimator) {
+	}
+
+	public void display(CCGraphics g) {
+	}
+}
