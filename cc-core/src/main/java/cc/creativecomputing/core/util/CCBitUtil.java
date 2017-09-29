7f89da5329edcb5d766367732c80065f787532ee
christian riekoff <info@texone.org>
2017-09-29 06:30:04 +0200

    added combine method to put for bytes to an int

--- cc-core/src/main/java/cc/creativecomputing/core/util/CCBitUtil.java
+++ cc-core/src/main/java/cc/creativecomputing/core/util/CCBitUtil.java
@@ -16,4 +16,8 @@ public class CCBitUtil {
 	public static int bit(int theValue, int theBit){
 		return ((theValue >>  theBit * 8) & 0xff);
 	}
+	
+	public static int combine(int the0, int the1, int the2, int the3) {
+		return (the3 << 24) | (the2 << 16) | (the1 << 8) | the0;
+	}
 }
