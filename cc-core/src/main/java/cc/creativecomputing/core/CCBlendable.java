c7de5b075cf80797fd104ab2714dcd338f20c9c7
christian riekoff <info@texone.org>
2017-09-29 06:30:17 +0200

    updated interfaces

--- cc-core/src/main/java/cc/creativecomputing/core/CCBlendable.java
+++ cc-core/src/main/java/cc/creativecomputing/core/CCBlendable.java
@@ -1,6 +1,9 @@
 package cc.creativecomputing.core;
 
-public interface CCBlendable<Type> {
+import cc.creativecomputing.core.io.format.CCDataSerializable;
+
+public interface CCBlendable<Type> extends CCDataSerializable{
+	public static final String BLENDABLE_TYPE_ATTRIBUTE = "blendable_type";
 
 	public Type blend(Type theB, double theScalar);
 	
--- cc-core/src/main/java/cc/creativecomputing/core/io/format/CCDataSerializable.java
+++ cc-core/src/main/java/cc/creativecomputing/core/io/format/CCDataSerializable.java
@@ -3,6 +3,9 @@ package cc.creativecomputing.core.io.format;
 import java.util.Map;
 
 public interface CCDataSerializable{
-	public Map<String, Object> toDataObject(CCDataHolder<?, ?> theHolder);
+
+	public void data(Map<String,Object> theData);
+	
+	public Map<String,Object> data();
 	
 }
