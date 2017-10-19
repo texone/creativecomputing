2be766f94d63953165ade6c1bdbf43d73c3ad055
christian riekoff <info@texone.org>
2017-10-19 08:32:36 +0200

    update nodeinput

--- cc-control-ui/src/main/java/cc/creativecomputing/controlui/patch/CCNodeInput.java
+++ cc-control-ui/src/main/java/cc/creativecomputing/controlui/patch/CCNodeInput.java
@@ -3,7 +3,7 @@ package cc.creativecomputing.controlui.patch;
 public class CCNodeInput<Type extends Object> {
 	public CCNodeOutput _myOutput;
 	
-	public Class<?> type(){
-		return Type.class;
-	}
+//	public Class<?> type(){
+//		return Type.class;
+//	}
 }
