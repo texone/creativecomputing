0e20d20ae6e3c8879c8300c422957ff08320e4e6
christian riekoff <info@texone.org>
2017-12-12 17:43:53 +0100

    added recompile option

--- cc-gl2.0/src/main/java/cc/creativecomputing/graphics/app/CCGL2Application.java
+++ cc-gl2.0/src/main/java/cc/creativecomputing/graphics/app/CCGL2Application.java
@@ -6,11 +6,15 @@ import cc.creativecomputing.app.modules.CCAnimator;
 import cc.creativecomputing.app.modules.CCAnimatorListener;
 import cc.creativecomputing.app.modules.CCAnimator.CCAnimationMode;
 import cc.creativecomputing.control.CCPropertyMap;
+import cc.creativecomputing.control.code.memorycompile.CCInMemoryExecutionManager;
+import cc.creativecomputing.control.handles.CCObjectPropertyHandle;
+import cc.creativecomputing.control.handles.CCPropertyHandle;
 import cc.creativecomputing.controlui.CCControlApp;
 import cc.creativecomputing.controlui.CCTimelineSynch;
 import cc.creativecomputing.core.CCProperty;
 import cc.creativecomputing.core.logging.CCLog;
 import cc.creativecomputing.gl.app.CCGLAdapter;
+import cc.creativecomputing.gl.app.CCGLListener;
 import cc.creativecomputing.graphics.CCGraphics;
 import cc.creativecomputing.io.CCNIOUtil;
 
@@ -22,45 +26,68 @@ public class CCGL2Application {
 	private CCGL2Context _myGLContext;
 	@CCProperty(name = "synch")
 	private CCTimelineSynch _mySynch;
-	
 	@CCProperty(name = "app")
 	private CCGLAdapter<CCGraphics, CCGL2Context> _myAdapter;
+	@CCProperty(name = "real compile")
+	private boolean _cRealCompile = false;
+	
+	private CCControlApp _myControlApp;
 	
 	public String presetPath = null;
 	
 	private boolean _myIsInitialized = false;
 	
 	private boolean _myUseUI;
+	
+	private CCInMemoryExecutionManager _myExecutionManager;
+	private CCGLAdapter<CCGraphics, CCGL2Context> _myOriginalCopy;
+	
+	private boolean _myUpdateUI = false;
 
 	public CCGL2Application(CCGLAdapter<CCGraphics, CCGL2Context> theGLAdapter, boolean useUI) {
 		_myUseUI = useUI;
 		_myAdapter = theGLAdapter;
+		_myOriginalCopy = theGLAdapter;
 		_myAnimator = new CCAnimator();
 		_myAnimator.framerate = 60;
 		_myAnimator.animationMode = CCAnimationMode.FRAMERATE_PRECISE;
 		
 		_mySynch = new CCTimelineSynch(_myAnimator);
 		
+		
 		_myAnimator.listener().add(new CCAnimatorListener() {
 			
 			@Override
 			public void update(CCAnimator theAnimator) {
-				if(_myIsInitialized)theGLAdapter.update(theAnimator);
+				if(!_myIsInitialized)return;
+				
+				if(_cRealCompile){
+
+					if(_myExecutionManager == null)_myExecutionManager = new CCInMemoryExecutionManager(theGLAdapter.getClass());
+					if(_myExecutionManager.update()){
+						if(_myExecutionManager.mainObject()!= null){
+							_myUpdateUI = true;
+						}
+					}
+				}else{
+					_myAdapter = _myOriginalCopy;
+				}
+				_myAdapter.update(theAnimator);
 			}
 			
 			@Override
 			public void stop(CCAnimator theAnimator) {
-				theGLAdapter.start(theAnimator);
+				_myAdapter.start(theAnimator);
 			}
 			
 			@Override
 			public void start(CCAnimator theAnimator) {
-				theGLAdapter.start(theAnimator);
+				_myAdapter.start(theAnimator);
 			}
 		});
 		
 		_myGLContext = new CCGL2Context(_myAnimator);
-		theGLAdapter.glContext(_myGLContext);
+		_myAdapter.glContext(_myGLContext);
 		if(presetPath == null){
 			presetPath = "settings/" + _myAdapter.getClass().getName() + "/";
 		}
@@ -96,7 +123,10 @@ public class CCGL2Application {
 		};
 		
 		
-		if(_myUseUI)myGLAdapter.controlApp(new CCControlApp(myGLAdapter, _mySynch, theGLAdapter.getClass()));
+		if(_myUseUI){
+			_myControlApp = new CCControlApp(myGLAdapter, _mySynch, theGLAdapter.getClass());
+			myGLAdapter.controlApp(_myControlApp);
+		}
 
 		_myGLContext.listener().add(myGLAdapter);
 		_myGLContext.listener().add(
@@ -107,7 +137,35 @@ public class CCGL2Application {
 				}
 			}
 		);
-		_myGLContext.listener().add(theGLAdapter);
+		_myGLContext.listener().add(new CCGLListener<CCGraphics>() {
+
+			@Override
+			public void reshape(CCGraphics g) {
+				_myAdapter.reshape(g);
+			}
+
+			@Override
+			public void init(CCGraphics g) {
+				_myAdapter.init(g);
+			}
+
+			@Override
+			public void dispose(CCGraphics g) {
+				_myAdapter.dispose(g);
+			}
+
+			@Override
+			public void display(CCGraphics g) {
+				_myAdapter.display(g);
+				if(_myUpdateUI){
+					_myUpdateUI = false;
+					_myAdapter = (CCGLAdapter<CCGraphics, CCGL2Context>)_myExecutionManager.mainObject();
+					_myAdapter.glContext(_myGLContext);
+					CCObjectPropertyHandle myProperty = (CCObjectPropertyHandle)_myControlApp.propertyMap().rootHandle().property("app");
+					myProperty.value(_myAdapter, true);
+				}
+			}
+		});
 		_myGLContext.listener().add(
 			new CCGLAdapter<CCGraphics, CCGL2Context>() {
 				@Override
